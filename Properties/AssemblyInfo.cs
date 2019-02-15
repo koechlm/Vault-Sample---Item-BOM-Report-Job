ffd2fc3f65b8a6a55b57cd49693f808f417af132
MKoe VisualStudio <markus.koechl@autodesk.com>
2019-02-15 14:46:07 +0100

    assembly info updated to 2019

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BOMReportJob 2018")]
+[assembly: AssemblyTitle("BOMReportJob 2019")]
 [assembly: AssemblyDescription("Creates / Attaches BOM Reports")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Autodesk")]
